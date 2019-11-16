9e66c61443efaddbe3e05a9d133b01a26463fcd4
Thanhramsey <%Thanhyth1%>
2019-11-16 18:42:25 +0700

    first

---
+++ js/carousel.js
@@ -0,0 +1,12 @@
+// $(function(){
+//     $("#img1").onclick(function () {
+//         $("#img1W").src("../images/clothes/áo/white/basic.jpg")
+//         $("#img1W").css("display","block");
+//         $("#img1").css("display","none");
+//     })
+//
+//     $("#img1").mouseleave(function () {
+//         $("#img1W").css("display","none");
+//         $("#img1").css("display","block");
+//     })
+// });
